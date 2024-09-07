3742106bab334c20f9ac50f794db9669694dcbeb
jose-lucas <joselucas.dev@gmail.com>
2024-09-07 17:51:24 -0300

    feat:add attrivutes to PostSchema.ts

--- back-end/src/db/schemas/PostSchema.ts
+++ back-end/src/db/schemas/PostSchema.ts
@@ -6,6 +6,8 @@ const { Schema } = mongoose;
 const postSchema = new Schema<IPost>({
     description: { type: String, required: true },
     title: { type: String, required: true },
+    createdAt: { type: Date, required: true },
+    updatedAt: { type: Date },
     owner: {
         type: Schema.Types.ObjectId,
         ref: 'User',
