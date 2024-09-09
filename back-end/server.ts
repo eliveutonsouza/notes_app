36035f08cc513ed2d13e44b781d347479b770afc
jose-lucas <joselucas.dev@gmail.com>
2024-09-09 14:16:00 -0300

    server:Server start just after connect database

--- back-end/server.ts
+++ back-end/server.ts
@@ -1,10 +1,21 @@
 import app from './src/app';
+import connect from './src/database/connection'; // Adjust the import path if necessary
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const port = process.env.PORT_APP || 3000;
 
-app.listen(port, () => {
-    console.log('app run on port 3000');
-});
+async function startServer() {
+    try {
+        await connect();
+        app.listen(port, () => {
+            console.log(`App running on port ${port}`);
+        });
+    } catch (error) {
+        console.error('Failed to start server:', error);
+        process.exit(1);
+    }
+}
+
+startServer();
