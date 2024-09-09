9a7a8389c276041c690be080f73f84f4ff4ffbe7
jose-lucas <joselucas.dev@gmail.com>
2024-09-09 14:14:20 -0300

    feat:createUser method

--- back-end/src/services/UserService/UserService.ts
+++ back-end/src/services/UserService/UserService.ts
@@ -1,7 +1,14 @@
 import IUser from '../../contracts/User';
-
+import User from '../../database/models/userModel';
 export default class UserService {
-    createUser(data: Omit<IUser, 'posts'>): String {
-        return 'user created';
+    async createUser(data: Omit<IUser, '_id'>): Promise<IUser> {
+        try {
+            const userInstance = new User(data);
+            const response = await userInstance.save();
+            return response;
+        } catch (err) {
+            console.log(err);
+            throw new Error('failed to save a user');
+        }
     }
 }
