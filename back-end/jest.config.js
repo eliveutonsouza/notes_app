ea09d7acdc3c66d9a687edf0966eb60f560c5e2a
jose-lucas <joselucas.dev@gmail.com>
2024-09-09 14:16:38 -0300

    feat:create config and UserCreateTest

--- back-end/jest.config.js
+++ back-end/jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
+    testTimeout: 20000,
     testEnvironment: 'node',
     transform: {
         '^.+.tsx?$': ['ts-jest', {}],
--- back-end/src/test/userTest.test.ts
+++ back-end/src/test/userTest.test.ts
@@ -2,28 +2,53 @@ import crypto from 'crypto';
 import IUser from '../contracts/User';
 import UserService from '../services/UserService/UserService';
 const axios = require('axios');
+import mongoose from 'mongoose';
+import db from '../database/connection';
+import dotenv from 'dotenv';
+import IPost from '../contracts/Post';
+
+dotenv.config();
 
 function generate(): String {
     return crypto.randomBytes(20).toString('hex');
 }
 
-async function request(url: String, method: String, data: IUser) {
+async function request(
+    url: String,
+    method: String,
+    data: Omit<IUser, 'posts'>
+) {
     return axios({ url, method, data });
 }
 
+beforeAll(async () => {
+    await db();
+});
+
+afterAll(async () => {
+    await mongoose.disconnect();
+});
+
 describe('create User', () => {
     test('should to create a user', async function () {
-        const data: Omit<IUser, 'posts'> = {
-            id: generate(),
+        const data: Omit<IUser, '_id'> = {
             name: generate(),
             email: generate(),
             password: generate(),
+            posts: new Array<IPost>(),
         };
 
         const userService = new UserService();
 
-        const response = userService.createUser(data);
+        const response: IUser = await userService.createUser(data);
 
-        expect(response).toBeDefined();
+        const responseNecessary = {
+            name: response.name,
+            email: response.email,
+            password: response.password,
+            posts: response.posts,
+        };
+
+        expect(responseNecessary).toStrictEqual(data);
     });
 });
