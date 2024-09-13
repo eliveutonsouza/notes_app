63f4b4a6a66a3d166dfe4f4211eeecfac8b9665e
jose-lucas <joselucas.dev@gmail.com>
2024-09-13 12:28:17 -0300

    feat:add service find ByEmail

--- back-end/src/services/UserService/UserService.ts
+++ back-end/src/services/UserService/UserService.ts
@@ -1,9 +1,10 @@
-import IUser from '../../contracts/User';
-import User from '../../database/models/userModel';
+import IUser from '../../contracts/IUser';
+import UserModel from '../../database/models/userModel';
+import { User } from '../../entity/User';
 export default class UserService {
     async createUser(data: Omit<IUser, '_id'>): Promise<IUser> {
         try {
-            const userInstance = new User(data);
+            const userInstance = new UserModel(data);
 
             const response = await userInstance.save();
 
@@ -11,6 +12,28 @@ export default class UserService {
         } catch (err) {
             console.log(err);
             throw new Error('failed to save a user');
+        }
+    }
+    async findByEmail(email: string): Promise<IUser> {
+        try {
+            const user = await UserModel.findOne({ email });
+
+            if (!user) {
+                throw new Error('user doesnt exist');
+            }
+
+            const userFromDB: IUser = new User(
+                user._id,
+                user.name,
+                user.email,
+                user.password,
+                user.posts
+            );
+
+            return userFromDB;
+        } catch (e) {
+            console.log(e);
+            throw new Error('Failed to find');
         }
     }
 }
