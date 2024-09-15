93a191236e7ebcd6a3b91867127e4a6a43790723
jose-lucas <joselucas.dev@gmail.com>
2024-09-15 12:43:56 -0300

    feat:middleware using Routes.ts

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import route from './routes/routes';
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(route);
 
 export = app;
