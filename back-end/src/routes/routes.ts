9d0927f6fa9936eccdc1642bcc20caa6ed60287e
jose-lucas <joselucas.dev@gmail.com>
2024-09-16 15:51:53 -0300

    feat:add post Routers

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -2,14 +2,23 @@ import { Router } from 'express';
 import authController from '../controllers/AuthController/authController';
 import { registerController } from '../controllers/UserController/Register/registerController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+
+import {
+    createPost,
+    deletePost,
+    getPosts,
+    updatePost,
+} from '../controllers/PostController/postController';
+
 const router = Router();
 
 router.post('/login', authController);
 
 router.post('/register', registerController);
 
-router.get('/', ensureAuthenticated, (req, res) => {
-    res.send('initial page');
-});
+router.post('/post', ensureAuthenticated, createPost);
+router.get('/post', ensureAuthenticated, getPosts);
+router.put('/post', ensureAuthenticated, updatePost);
+router.delete('/post', ensureAuthenticated, deletePost);
 
 export default router;
