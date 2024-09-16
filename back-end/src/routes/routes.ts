29935edd44cbb5dc1c31da47a9bcc041582f5f44
jose-lucas <joselucas.dev@gmail.com>
2024-09-16 13:19:23 -0300

    feat:ensureAuthenticated middleware Worksss !!!!

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import authController from '../controllers/AuthController/authController';
 import { registerController } from '../controllers/UserController/Register/registerController';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const router = Router();
 
 router.post('/login', authController);
 
 router.post('/register', registerController);
+
+router.get('/', ensureAuthenticated, (req, res) => {
+    res.send('initial page');
+});
 
 export default router;
