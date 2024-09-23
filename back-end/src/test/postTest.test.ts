ecc407ae4bb8e24dd16e4f526d2376f6a2ad08e5
jose-lucas <joselucas.dev@gmail.com>
2024-09-23 15:05:11 -0300

    test:feat refactor postTest

--- back-end/src/test/postTest.test.ts
+++ back-end/src/test/postTest.test.ts
@@ -1,12 +1,14 @@
 import crypto from 'crypto';
 
-import axios from 'axios';
-import mongoose from 'mongoose';
+// import axios from 'axios';
+import mongoose, { Schema } from 'mongoose';
 import db from '../database/connection';
 import dotenv from 'dotenv';
 import IPost from '../contracts/IPost';
+import IUser from '../contracts/IUser';
 import PostService from '../services/PostService/PostService';
 import UserService from '../services/UserService/UserService';
+import PaginationIPost from '../contracts/PaginationIPost';
 
 dotenv.config();
 
@@ -14,95 +16,140 @@ function generate(): string {
     return crypto.randomBytes(20).toString('hex');
 }
 
-async function request(
-    url: string,
-    method: string,
-    data: Omit<IPost, 'posts'>
-) {
-    return axios({ url, method, data });
-}
+const userData: Omit<IUser, '_id'> = {
+    name: 'testusername',
+    email: 'test@hotmail.com',
+    password: 'password',
+    posts: new Array<IPost>(),
+};
+
+// async function request(
+//     url: string,
+//     method: string,
+//     data: Omit<IPost, 'posts'>
+// ) {
+//     return axios({ url, method, data });
+// }
 const userService = new UserService();
 const postService = new PostService();
 
+const postData: Omit<IPost, '_id'> = {
+    title: generate(),
+    description: generate(),
+    owner: new Schema.ObjectId(''),
+    updatedAt: undefined,
+    createdAt: new Date(),
+};
+
 beforeAll(async () => {
     await db();
+    await userService.createUser(userData);
 });
 
 afterAll(async () => {
+    await userService.deleteUser(userData.email);
     await mongoose.disconnect();
 });
 
 describe('create a Post', () => {
     test('should to create a post', async function () {
-        const data: Omit<IPost, '_id'> = {
-            title: generate(),
-            description: generate(),
-            owner: '66e60d2930b2f68d8bd67de9',
-            updatedAt: undefined,
-            createdAt: new Date(),
-        };
-
-        const response: IPost = await postService.createPost(data);
+        const response: IPost = await postService.createPost(
+            postData,
+            userData.email
+        );
 
         const responseNecessary = {
             title: response.title,
             description: response.description,
-            owner: response.owner.toString(),
-            updatedAt: response.updatedAt,
-            createdAt: response.createdAt,
         };
 
-        console.log(responseNecessary, data);
+        expect(response).toBeDefined();
 
-        expect(responseNecessary).toStrictEqual(data);
+        expect(responseNecessary.title).toStrictEqual(postData.title);
+        expect(responseNecessary.description).toStrictEqual(
+            postData.description
+        );
+
+        await postService.deletePost(response._id, userData.email);
     });
+});
 
-    describe('update a post', () => {
-        test('should to update a post', async function () {
-            const data: Pick<IPost, 'title' | 'description' | 'updatedAt'> = {
-                title: generate(),
-                description: generate(),
-                updatedAt: new Date(),
-            };
-            const usr = await userService.findByEmail(
-                'a2882b7be69010696ee82aa58dc4b3597ff5a08f'
-            );
+describe('get Posts by Title', () => {
+    test('should to get post by Title', async function () {
+        const post1 = await postService.createPost(postData, userData.email);
+        const post2 = await postService.createPost(postData, userData.email);
 
-            const postResponse = await postService.updatePost(
-                data,
-                usr.posts[0]._id
-            );
+        const response: IPost[] = await postService.getPostByTitle(
+            userData.email,
+            postData.title
+        );
 
-            const responseNecessary = {
-                title: postResponse.title,
-                description: postResponse.description,
-            };
+        expect(response[0].title).toStrictEqual(postData.title);
+        expect(response[1].title).toStrictEqual(postData.title);
 
-            console.log(responseNecessary, data);
+        await postService.deletePost(post1._id, userData.email);
+        await postService.deletePost(post2._id, userData.email);
+    });
+});
 
-            expect(responseNecessary).toStrictEqual(data);
-        });
+describe('get Posts', () => {
+    test('should to get all posts', async function () {
+        const post1 = await postService.createPost(postData, userData.email);
+
+        const post2 = await postService.createPost(postData, userData.email);
+
+        await postService.createPost(postData, userData.email);
+
+        const response: PaginationIPost = await postService.getAllPosts(
+            userData.email,
+            1
+        );
+
+        expect(response.posts.length).toEqual(3);
+
+        await postService.deletePost(post1._id, userData.email);
+        await postService.deletePost(post2._id, userData.email);
     });
 });
 
 describe('delete a post', () => {
-    test.only('should to delete a post', async function () {
-        const usr = await userService.findByEmail(
-            '35479f6873837b113c0a58575c8d5569cf24088e'
+    test('should to delete a post', async function () {
+        const responseCreate = await postService.createPost(
+            postData,
+            userData.email
         );
 
-        const initialPostCount = usr.posts.length;
-
-        expect(initialPostCount).toBeGreaterThan(0);
-
-        const postId = usr.posts[0]._id;
-
-        await postService.deletePost(postId);
-
-        const updatedUsr = await userService.findByEmail(
-            '35479f6873837b113c0a58575c8d5569cf24088e'
+        const respDel = await postService.deletePost(
+            responseCreate._id,
+            userData.email
         );
 
-        expect(updatedUsr.posts.length).toBe(initialPostCount - 1);
+        expect(respDel).toBeDefined();
+        expect(respDel._id).toStrictEqual(responseCreate._id);
+    });
+});
+
+describe('update a post', () => {
+    test('should to update a post', async function () {
+        const responseCreate = await postService.createPost(
+            postData,
+            userData.email
+        );
+        const newPost: Pick<IPost, 'title' | 'updatedAt' | 'description'> = {
+            title: 'newTitle',
+            updatedAt: undefined,
+            description: 'new Description',
+        };
+        const postUpdated = await postService.updatePost(
+            newPost,
+            responseCreate._id,
+            userData.email
+        );
+
+        expect(postUpdated).toBeDefined();
+        expect(postUpdated.title).toStrictEqual(newPost.title);
+        expect(postUpdated.description).toStrictEqual(newPost.description);
+
+        await postService.deletePost(responseCreate._id, userData.email);
     });
 });
