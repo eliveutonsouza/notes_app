eafb6d5f693ffa6903d632e60d7ab50a12517ea8
jose-lucas <joselucas.dev@gmail.com>
2024-09-26 08:49:30 -0300

    feat:add validation colorHex

--- back-end/src/services/PostService/validations/PostValidation.ts
+++ back-end/src/services/PostService/validations/PostValidation.ts
@@ -1,9 +1,10 @@
 import IPost from '../../../contracts/IPost';
 
 export default class PostValidation {
-    validation(data: Pick<IPost, 'title' | 'description'>): void {
+    validation(data: Pick<IPost, 'title' | 'description' | 'colorHex'>): void {
         this.titleValidation(data.title);
         this.descriptionValidation(data.description);
+        this.colorHexValidation(data.colorHex);
     }
 
     private titleValidation(title: string): void {
@@ -12,5 +13,15 @@ export default class PostValidation {
 
     private descriptionValidation(description: string): void {
         if (!description) throw new Error('desc cannot be null');
+    }
+
+    private colorHexValidation(colorHex: string): void {
+        if (!colorHex) throw new Error('ColorHex cannot be null');
+
+        const rgxHexColor = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+
+        if (!rgxHexColor.test(colorHex)) {
+            throw new Error('color invalid');
+        }
     }
 }
