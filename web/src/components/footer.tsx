aeb0a00dc1d97ea1ebe8e102310f9e7fa4f0cdc9
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-10-06 18:12:14 -0300

    feat: create footer

---
+++ web/src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="bg-black h-20 text-white flex items-center justify-center">
+      <p>© {new Date().getFullYear()} - Todos os direitos reservados</p>
+    </footer>
+  );
+}
--- web/src/layouts/DashboardLayouts.tsx
+++ web/src/layouts/DashboardLayouts.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/header";
+import { Hero } from "../components/hero";
+import { Footer } from "../components/footer";
 
 export function DashboardLayouts() {
   return (
     // Outro componente pode ser renderizado aqui
-    <div className="bg-white">
-      <Header />
-      <Outlet />
+    <div className="h-screen bg-white flex flex-col justify-between item">
+      <div>
+        <Header />
+        <Hero />
+        <Outlet />
+      </div>
+
+      <Footer />
     </div>
   );
 }
