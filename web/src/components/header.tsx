4c2658b5708ddf67e2cc33f667ef09c04b9b524c
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-10-06 18:13:20 -0300

    refactor: change localization and structure

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -3,7 +3,7 @@ import logoFull from "../assets/logo-full.png";
 
 export function Header() {
   return (
-    <header className="container py-5 m-auto flex justify-between items-center">
+    <header className="container py-4 m-auto flex justify-between items-center">
       <img src={logoFull} alt="Logo do aplicativo Notes" />
 
       <div className="flex justify-center items-center gap-4">
