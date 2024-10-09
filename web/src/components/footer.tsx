42e860d5fdfbba9d9876df5ca56c70a59d265cf6
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-10-08 22:52:58 -0300

    bug: fix footer layout

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="bg-black h-20 text-white flex items-center justify-center">
+    <footer className="bg-black py-7 text-white flex items-center justify-center">
       <p>© {new Date().getFullYear()} - Todos os direitos reservados</p>
     </footer>
   );
