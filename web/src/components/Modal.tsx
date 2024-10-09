19773ab84d4e1dc2acff110849bffb78b279cae1
Eliveuton Souza <eliveuton3m@hotmail.com>
2024-10-08 22:54:31 -0300

    feat: add button close in all components modal

--- web/src/components/Modal.tsx
+++ web/src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useRef } from "react";
 import { ModalContext } from "../context/ModalContextProvider";
+import { X } from "@phosphor-icons/react";
 
 interface ModalProps {
   id: string;
@@ -7,7 +8,7 @@ interface ModalProps {
 }
 
 export function Modal({ id, children }: ModalProps) {
-  const { isOpen } = useContext(ModalContext);
+  const { isOpen, close } = useContext(ModalContext);
   const modalRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -31,13 +32,19 @@ export function Modal({ id, children }: ModalProps) {
   return (
     <div
       ref={modalRef}
-      className="fixed inset-0 z-50 bg-black bg-opacity-70 flex items-center justify-center"
+      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70"
       style={{ display: isOpen(id) ? "flex" : "none" }}
+      onClick={() => close(id)} // Fechar modal ao clicar fora do conteúdo
     >
-      <div className="w-full h-full flex items-center justify-center">
-        <div className="w-[40rem] bg-white py-16 px-24 border-t-8 border-lime-300 rounded">
-          {children}
-        </div>
+      <div
+        className="relative w-[40rem] rounded border-t-8 border-lime-300 bg-white px-24 py-16"
+        onClick={(e) => e.stopPropagation()} // Impedir propagação do clique no conteúdo
+      >
+        <button onClick={() => close(id)} className="absolute right-4 top-4">
+          <span className="sr-only">Fechar modal</span>
+          <X size={24} />
+        </button>
+        {children}
       </div>
     </div>
   );
