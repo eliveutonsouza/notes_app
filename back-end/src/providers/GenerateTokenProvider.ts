aa3fb70e119f535fd82cf7e6aa3ad0931e3272ac
jose-lucas <joselucas.dev@gmail.com>
2024-10-13 21:29:14 -0300

    add username on payload jwt

--- back-end/src/providers/GenerateTokenProvider.ts
+++ back-end/src/providers/GenerateTokenProvider.ts
@@ -5,7 +5,7 @@ class GenerateTokenProvider {
         const expiresIn = 86400;
         const privateKeyJwt = process.env.PRIVATE_KEY_JWT;
         const token = sign(
-            {},
+            { userName: userName },
             privateKeyJwt || 'e2ac90fa-2e12-4f99-8dde-7e3a7690ab35',
             {
                 subject: userEmail,
