86676db0bb57ad642fd96d1d02aee2934f9f64a5
jose-lucas <joselucas.dev@gmail.com>
2024-10-20 15:10:37 -0300

    update:prod connection

--- back-end/src/database/connection.ts
+++ back-end/src/database/connection.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 async function connect(): Promise<void> {
-    const connectionUrl = process.env.CONNECTION_URL_LOCAL;
+    const connectionUrl = process.env.CONNECTION_URL_PROD;
 
     if (!connectionUrl) {
         process.exit(-1);
